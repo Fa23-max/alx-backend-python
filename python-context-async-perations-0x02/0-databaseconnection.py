c6e48022d5330d2b5d304944daa3c0bee7a13823
Fa23-max <musilijose618@gmail.com>
2025-07-11 14:59:03 +0300

    context

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+class DatabaseConnection:
+    def _init_(self, db_name):
+        self.db_name = db_name
+        self.connection = None
+    def _enter_(self):
+        self.connection = sqlite3.connect(self.db_name)
+        return self.connection
+    def _exit_(self, exc_type, exc_value, traceback):
+        if self.connection:
+            self.connection.close()
+        if exc_type is not None:
+            print(f"An error occurred: {exc_value}")
+        return False
+
+with DatabaseConnection('users.db') as conn:
+    c = conn.cursor()
+    c.execute("SELECT * FROM users")
+    print(c.fetchall())

608bf328a10c503915220866213b9108db61e26d
Fa23-max <musilijose618@gmail.com>
2025-07-25 09:08:01 +0300

    django permissions

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,8 +1,4 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from .models import Conversation, Message
-from .serializers import MessageSerializer
-from .permissions import IsParticipantOfConversation, IsOwnerOrAdmin
+
 from rest_framework import permissions
 
 class IsOwnerOrAdmin(permissions.BasePermission):
@@ -65,26 +61,3 @@ class IsParticipantOfConversation(permissions.BasePermission):
         return False
 # ["conversation_id", "Message.objects.filter"]
 "conversation_id", "Message.objects.filter"
-
-
-class MessageViewSet(viewsets.ModelViewSet):
-    serializer_class = MessageSerializer
-    permission_classes = [IsParticipantOfConversation, IsOwnerOrAdmin]
-    
-    def get_queryset(self):
-        # Filter messages based on conversation_id if provided in query params
-        conversation_id = self.request.query_params.get('conversation_id')
-        if conversation_id:
-            return Message.objects.filter(conversation_id=conversation_id)
-        return Message.objects.none()  # Return empty if no conversation_id provided
-    
-    def perform_create(self, serializer):
-        conversation_id = self.request.data.get('conversation_id')
-        try:
-            conversation = Conversation.objects.get(id=conversation_id)
-            if self.request.user in conversation.participants.all():
-                serializer.save(user=self.request.user, conversation=conversation)
-            else:
-                raise PermissionDenied("You are not a participant of this conversation")
-        except Conversation.DoesNotExist:
-            raise NotFound("Conversation not found")
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -4,6 +4,7 @@ from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 from .permissions import IsParticipantOfConversation
 from rest_framework.response import Response
+from .permissions import IsParticipantOfConversation, IsOwnerOrAdmin
 
 class ConversationViewSet(viewsets.ModelViewSet):
     """
@@ -53,3 +54,35 @@ class MessageViewSet(viewsets.ModelViewSet):
                 status=status.HTTP_403_FORBIDDEN
             )
         serializer.save(sender=self.request.user)
+
+
+# from rest_framework import viewsets, status
+# from rest_framework.response import Response
+# from .models import Conversation, Message
+# from .serializers import MessageSerializer
+# from .permissions import IsParticipantOfConversation, IsOwnerOrAdmin
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    permission_classes = [IsParticipantOfConversation, IsOwnerOrAdmin]
+    
+    def get_queryset(self):
+        # Filter messages based on conversation_id if provided in query params
+        conversation_id = self.request.query_params.get('conversation_id')
+        if conversation_id:
+            return Message.objects.filter(conversation_id=conversation_id)
+        return Message.objects.none()  # Return empty if no conversation_id provided
+    
+    def perform_create(self, serializer):
+        conversation_id = self.request.data.get('conversation_id')
+        try:
+            conversation = Conversation.objects.get(id=conversation_id)
+            if self.request.user in conversation.participants.all():
+                serializer.save(user=self.request.user, conversation=conversation)
+            else:
+                raise PermissionDenied("You are not a participant of this conversation")
+        except Conversation.DoesNotExist:
+            raise NotFound("Conversation not found")
+
+
+# ["conversation_id", "Message.objects.filter"]

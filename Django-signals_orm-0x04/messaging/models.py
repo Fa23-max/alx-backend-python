2ab91c6037a63cd0fe8e604d23151875abc4e6ae
Fa23-max <musilijose618@gmail.com>
2025-08-03 09:06:48 +0300

    alx backend

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -6,6 +6,7 @@ class Message(models.Model):
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)  # New field to track edits
 
     def __str__(self):
         return f'Message from {self.sender} to {self.receiver} at {self.timestamp}'
@@ -19,3 +20,12 @@ class Notification(models.Model):
 
     def __str__(self):
         return f'Notification for {self.user.username} - Message ID: {self.message.id}'
+
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Edit of Message ID {self.message.id} at {self.edited_at}'
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,11 +1,23 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
     if created:
-        Notification.objects.create(
-            user=instance.receiver,
-            message=instance
-        )
+        Notification.objects.create(user=instance.receiver, message=instance)
+
+
+@receiver(pre_save, sender=Message)
+def log_message_history(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            old_message = Message.objects.get(pk=instance.pk)
+            if old_message.content != instance.content:
+                MessageHistory.objects.create(
+                    message=old_message,
+                    old_content=old_message.content
+                )
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass
